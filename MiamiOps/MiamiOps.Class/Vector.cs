9df2f58c9bd65d49339d87d5b48b173130ba81ca
Keftcha <alban.dewilde@gmail.com>
2018-07-09 18:10:47 +0200

    Add the project we made in S3

---
+++ MiamiOps/MiamiOps.Class/Vector.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MiamiOps
+{
+public struct Vector
+    {
+        private double _abs;
+        private double _ord;
+
+        public Vector(double abs, double ord)
+        {
+            this._abs = abs;
+            this._ord = ord;
+        }
+
+        public double X =>this._abs;
+        public double Y => this._ord;
+
+        public double Magnitude => Math.Sqrt(this._abs * this._abs + this._ord * this._ord);
+        public double MagnitudeInverse => Math.Sqrt(this._abs * this._abs + this._ord * this._ord);
+        
+
+        public static Vector operator+(Vector v1, Vector v2) {return new Vector(v1._abs + v2._abs, v1._ord + v2._ord);}
+        public static Vector operator-(Vector v1, Vector v2) {return new Vector(v1._abs - v2._abs, v1._ord - v2._ord);}
+        public static Vector operator*(Vector v1, double nb) {return new Vector(v1._abs * nb, v1._ord * nb);}
+        public static Vector operator*(double nb, Vector v1) {return v1 * nb;}
+        public static Vector operator/(Vector v1, double nb) {return new Vector(v1._abs / nb, v1._ord / nb);}
+
+        public float Distance(Vector v1, Vector v2)
+        {
+           float distance = (float)Math.Sqrt(Math.Pow(2, v2.X - v1.X) + Math.Pow(2, v2.Y - v1.Y));
+           return distance;
+        }
+        
+        
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 Semestre 1: _Réalisation d’un site web_ (multi-pages et SPA)
     **Environnement Technique:** NODE.JS (v8.9.3), HTML5, CSS3, Git
+
+Semestre 3: _Réalisation d’un jeu_ (type Beat’Em All)
+    **Environement Technique:** C#/.NET (net461), SFML (2.2.0), Git
